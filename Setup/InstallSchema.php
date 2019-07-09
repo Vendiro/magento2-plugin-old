cbad4f5b80a2f3ebe803a25096bda3f9507c6984
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-07-09 15:44:30 +0200

    VEN-28 added setup script for the order table

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Vendiro\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * @var array
+     */
+    private $installSchemaObjects;
+
+    /**
+     * @param array $installSchemaObjects
+     */
+    public function __construct(
+        $installSchemaObjects = []
+    ) {
+        $this->installSchemaObjects = $installSchemaObjects;
+    }
+
+    /**
+     * Installs DB schema for a module
+     *
+     * @param SchemaSetupInterface   $setup
+     * @param ModuleContextInterface $context
+     *
+     * @return void
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '1.1.0', '<')) {
+            $this->installSchemas($this->installSchemaObjects['v1.1.0'], $setup, $context);
+        }
+
+        $setup->endSetup();
+    }
+
+    /**
+     * @param $schemaObjects
+     * @param $setup
+     * @param $context
+     */
+    private function installSchemas($schemaObjects, $setup, $context)
+    {
+        /** @var AbstractTableInstaller|AbstractColumnInstaller $schema */
+        foreach ($schemaObjects as $schema) {
+            $schema->install($setup, $context);
+        }
+    }
+}
