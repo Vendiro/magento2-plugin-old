355a6e2062458169ba7a764a023b79de9228772a
Mickey Beijer <mickey.beijer@tig.nl>
2019-07-22 11:25:36 +0200

    VEN-38 : shipment carrier added

---
+++ Model/Carrier/Vendiro.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to support@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact support@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Vendiro\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+
+class Vendiro extends AbstractCarrier implements CarrierInterface
+{
+    protected $_code = 'tig_vendiro';
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function collectRates(RateRequest $request)
+    {
+        // TODO: Implement collectRates() method.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAllowedMethods()
+    {
+        return ['tig_vendiro' => $this->getConfigData('name')];
+    }
+}
