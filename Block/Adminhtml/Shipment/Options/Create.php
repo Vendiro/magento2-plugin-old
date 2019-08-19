b95305513050c9bfd4d5520e184b6a5efe02f2a4
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-08-19 15:36:09 +0200

    VEN-34 added carrier option when manually making a shipment

---
+++ Block/Adminhtml/Shipment/Options/Create.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Vendiro\Block\Adminhtml\Shipment\Options;
+
+use Magento\Backend\Block\Template;
+use Magento\Framework\View\Element\BlockInterface;
+use TIG\Vendiro\Model\CarrierRepository;
+
+class Create extends Template implements BlockInterface
+{
+    /**
+     * @var CarrierRepository
+     */
+    private $carrierRepository;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param CarrierRepository                       $carrierRepository
+     * @param array                                   $data
+     */
+    public function __construct(
+        Template\Context $context,
+        CarrierRepository $carrierRepository,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->carrierRepository = $carrierRepository;
+    }
+
+    /**
+     * @return \Magento\Framework\DataObject[]
+     */
+    public function getItems()
+    {
+       $collection = $this->carrierRepository->getCollection();
+       $collection = $collection->create();
+
+       return $collection->getItems();
+    }
+}
