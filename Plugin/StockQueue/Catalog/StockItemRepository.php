87b3d7cf854f85f5c12c5efb31c212bdad3ea4ff
Mickey Beijer <mickey.beijer@tig.nl>
2019-09-09 19:15:11 +0200

    VEN-22 : Product SKU is now queued when a product is being saved

--- Plugin/StockQueue/Catalog/StockItemRepository.php
+++ Plugin/StockQueue/Catalog/StockItemRepository.php
@@ -31,12 +31,78 @@
  */
 namespace TIG\Vendiro\Plugin\StockQueue\Catalog;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use TIG\Vendiro\Api\Data\StockInterface;
+use TIG\Vendiro\Api\StockRepositoryInterface;
+use TIG\Vendiro\Logging\Log;
+use TIG\Vendiro\Model\Config\Provider\ApiConfiguration;
+use TIG\Vendiro\Model\Config\Provider\QueueStatus;
 
 class StockItemRepository
 {
+    /** @var ApiConfiguration */
+    private $apiConfiguration;
+
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+
+    /** @var StockRepositoryInterface */
+    private $stockRepository;
+
+    /** @var Log */
+    private $logger;
+
+    public function __construct(
+        ApiConfiguration $apiConfiguration,
+        ProductRepositoryInterface $productRepository,
+        StockRepositoryInterface $stockRepository,
+        Log $logger
+    ) {
+        $this->apiConfiguration = $apiConfiguration;
+        $this->productRepository = $productRepository;
+        $this->stockRepository = $stockRepository;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param StockItemRepositoryInterface $subject
+     * @param                              $result
+     *
+     * @return mixed
+     */
+    // @codingStandardsIgnoreLine
     public function afterSave(StockItemRepositoryInterface $subject, $result)
     {
+        try {
+            $productId = $result->getProductId();
+            $magentoProduct = $this->productRepository->getById($productId);
+            $vendiroStock = $this->createVendiroStock($magentoProduct->getSku());
+            $this->stockRepository->save($vendiroStock);
+        } catch (NoSuchEntityException $exception) {
+            $errorMessage = 'Stock queue on Product #' . $productId . ' went wrong: ' . $exception->getMessage();
+            $this->logger->critical($errorMessage);
+        } catch (CouldNotSaveException $exception) {
+            $errorMessage = 'Stock queue on Product #' . $productId . ' went wrong: ' . $exception->getMessage();
+            $this->logger->critical($errorMessage);
+        }
+
         return $result;
+    }
+
+    /**
+     * @param string $sku
+     *
+     * @return StockInterface
+     */
+    private function createVendiroStock($sku)
+    {
+        $vendiroStock = $this->stockRepository->create();
+        $vendiroStock->setProductSku($sku);
+        $vendiroStock->setStatus(QueueStatus::QUEUE_STATUS_NEW);
+
+        return $vendiroStock;
     }
 }
