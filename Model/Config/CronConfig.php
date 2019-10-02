a42e83b70729700329da1797c47856b77198278f
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-10-02 16:44:13 +0200

    VEN-69 - Added cron to system.xml

---
+++ Model/Config/CronConfig.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ *
+ *          ..::..
+ *     ..::::::::::::..
+ *   ::'''''':''::'''''::
+ *   ::..  ..:  :  ....::
+ *   ::::  :::  :  :   ::
+ *   ::::  :::  :  ''' ::
+ *   ::::..:::..::.....::
+ *     ''::::::::::::''
+ *          ''::''
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright   Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Vendiro\Model\Config;
+
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Exception\CronException;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+use Magento\Config\Model\ResourceModel\Config as ConfigWriter;
+
+class CronConfig extends Value
+{
+    /** Cron string path */
+    const CRON_STRING_PATH = 'crontab/default/jobs/%s/schedule/cron_expr';
+
+    /** @var ConfigWriter  */
+    private $configWriter;
+
+    /**
+     * CronConfig constructor.
+     *
+     * @param Context               $context
+     * @param Registry              $registry
+     * @param ScopeConfigInterface  $config
+     * @param TypeListInterface     $cacheTypeList
+     * @param AbstractResource|null $resource
+     * @param ConfigWriter          $configWriter
+     * @param AbstractDb|null       $resourceCollection
+     * @param array                 $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        ConfigWriter $configWriter,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->configWriter = $configWriter;
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return $this
+     * @throws \Exception
+     */
+    public function afterSave()
+    {
+        $results = preg_split('#\s+#', $this->getValue(), null, PREG_SPLIT_NO_EMPTY);
+
+        if (sizeof($results) < 5 || sizeof($results) > 6) {
+            throw new CronException(__('Invalid cron expression: %1', $this->getValue()));
+        }
+
+        $cron = str_replace('%s', $this->getField(), CronConfig::CRON_STRING_PATH);
+
+        $this->configWriter->saveConfig($cron, $this->getValue());
+
+        return parent::afterSave();
+    }
+}
